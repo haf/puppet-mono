61261822676296eae12a03e851163040b11e41cd
Henrik <henrik@haf.se>
2013-12-01 11:41:32 +0100

    actual init.pp file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,33 @@
 class mono {
-  anchor { 'mono::start': } ->
-  package { 'mono': ensure => installed } ->
+  anchor { 'mono::start': }
+
+  package { 'mono':
+    ensure => installed,
+    require => Anchor['mono::start'],
+    before  => Anchor['mono::end'],
+  }
+
+  Exec {
+    user    => 'root',
+    path    => '/usr/bin:/bin',
+    require => [
+      Anchor['mono::start'],
+      Package['mono']
+    ],
+    before  => Anchor['mono::end'],
+  }
+
+  # http://www.mono-project.com/FAQ:_Security
+  exec {
+    'yes | certmgr -ssl -m https://nuget.org && touch /etc/.certs-nuget.org':
+      creates => '/etc/.certs-nuget.org';
+    'yes | certmgr -ssl -m https://packages.nuget.org/v1/FeedService.svc/ && touch /etc/.certs-packages.nuget.org':
+      creates => '/etc/.certs-packages.nuget.org';
+    'yes | certmgr -ssl -m https://go.microsoft.com && touch /etc/.certs-go.microsoft.com':
+      creates => '/etc/.certs-go.microsoft.com';
+    'yes | certmgr -ssl -m https://nugetgallery.blob.core.windows.net && touch /etc/.certs-nugetgallery.blob.core.windows.net':
+      creates => '/etc/.certs-nugetgallery.blob.core.windows.net';
+  }
+
   anchor { 'mono::end': }
 }
